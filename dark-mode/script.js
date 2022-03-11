877f47b8c150dce042993d06bd3b8e41f10a6fa6
지수민 <wltn1873@gmail.com>
2022-03-12 03:05:49 +0900

    Update dark mode

--- dark-mode/script.js
+++ dark-mode/script.js
@@ -1,5 +1,9 @@
 const lyrics = document.querySelector(".lyrics");
 
+if (window.matchMedia("(prefers-color-scheme:dark)")) {
+  lyrics.classList.add("dark");
+}
+
 window.addEventListener("click", () => {
   lyrics.classList.toggle("dark");
 });
